071837110cd623fcd0944e4981f2e65581edeb11
Thanakon46 <sarty6658@gmail.com>
2024-11-25 16:01:30 +0700

    65312009

--- question3-3.dart
+++ question3-3.dart
@@ -1,7 +1,17 @@
+import 'dart:io';
 void createUser(String name, int age, {bool isActive = true}) {
   print('Name: $name, Age: $age, Active: $isActive');
 }
 void main() {
-  createUser('ยอน', 25); 
-  createUser('วอฮาย', 30 ); 
+  print('ชื่อ:');
+  String? name = stdin.readLineSync();
+  print('อายุ:');
+  String? ageInput = stdin.readLineSync();
+  int age = int.tryParse(ageInput ?? '') ?? 0; // ใช้ค่าเริ่มต้น 0 หากผู้ใช้ไม่กรอกหรือกรอกค่าที่ไม่ถูกต้อง
+  print('ใช้งานอยู่รึป่าว (yes/no):');
+  String? isActiveInput = stdin.readLineSync()?.toLowerCase();
+  bool isActive = isActiveInput == 'yes' || isActiveInput == 'y';
+  if (name != null && name.isNotEmpty && age > 0) {
+    createUser(name, age, isActive: isActive);
+    }
 }
